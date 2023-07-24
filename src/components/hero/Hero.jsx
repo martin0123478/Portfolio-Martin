278876b73c989f8d330dab66ed060d73f8cd3fe3
martin0123478 <martin_agui97@outlook.es>
2023-07-23 21:47:30 -0600

    feat:create hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,12 +2,15 @@ import './hero.css'
 import logo from '../../assets/images/Martín.jpg'
 const Hero = () => {
     return (
-        <div className='container'>
-            <div className='info'>
+        <div className='container-hero'>
 
-            </div>
             <div className='foto'>
                 <img src={logo} alt="foto" />
+            </div>
+            <div className='info'>
+                <p className='hero-text'>Hola ✋, Soy Martín Aguilar un Frontend, centrado en la creación de productos digitales durante los últimos 2 años.</p>
+                <p className='hero-description'>Me encanta centrarme en los detalles de las experiencias de nuevos productos con una  implementación en código perfecta. Pero los grandes productos no comienzan ahí, comienzan con una sólida colaboración con los compañeros de equipo de investigación, ingeniería y productos para comprender los problemas y las circunstancias de los clientes.</p>
+                <button className='cv'>Descargar currículum</button>
             </div>
         </div>
 
